a0cc9015011c1ee4c42d38293ce66af07732c933
magdapaicu <102292971+magdapaicu@users.noreply.github.com>
2023-04-11 10:58:50 +0300

    Controller and Model for User Authenticate

---
+++ Login_SignUpPage_Backend/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Login_SignUpPage_Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Login_SignUpPage_Backend.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { 
+          modelBuilder.Entity<User>().ToTable("users");
+        }
+    }
+}
